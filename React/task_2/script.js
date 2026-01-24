a74818ee435b6e8fdaed5f91038ce7c90093dd29
Shulpov Victor <vshulpov@gmail.com>
2026-01-24 14:34:45 +0700

    vitya

--- React/task_2/script.js
+++ React/task_2/script.js
@@ -1,6 +1,7 @@
 const addElementBtn = document.getElementById("addElementId");
 const cardsWrapper = document.getElementById("cardsWrapperId");
 
+// СОЗДАНИЕ ЭЛЕМЕНТА
 const addElementFunc = function addElement() {
   const cardBlock = document.createElement("div");
   cardBlock.classList.add("card", "kub-card");
@@ -20,15 +21,31 @@ const addElementFunc = function addElement() {
   const editBtn = document.createElement("a");
   editBtn.textContent = "Редактировать";
   editBtn.classList.add("btn", "btn-warning");
+  editBtn.addEventListener("click", function() {
+    const editMenu = document.createElement("div");
+    const element_H = document.createElement("input");
+    const element_P = document.createElement("input");
+    const edit_apply_btn = document.createElement("button")
+    edit_apply_btn.classList.add("btn", "btn-success");
+    element_H.placeholder = ('Заголовок')
+    element_P.placeholder = ('Текст')
+    cardBody.appendChild(editMenu)
+    editMenu.appendChild(element_H);
+    editMenu.appendChild(element_P);
+    cardBody.appendChild(edit_apply_btn);
+  });
 
-  const deleteBtn = document.createElement("a");
+  const deleteBtn = document.createElement("button");
   deleteBtn.textContent = "Удалить";
   deleteBtn.classList.add("btn", "btn-danger");
+  deleteBtn.addEventListener("click", function() {
+    cardBlock.remove();
+  });
 
   //TODO то что внутри
+
   cardBody.appendChild(cardTitle);
   cardBody.appendChild(cardText);
-
 
 
   cardBlock.appendChild(cardBody);
@@ -37,4 +54,5 @@ const addElementFunc = function addElement() {
   cardBody.appendChild(deleteBtn);
   cardsWrapper.appendChild(cardBlock);
 };
+
 addElementBtn.addEventListener("click", addElementFunc);
